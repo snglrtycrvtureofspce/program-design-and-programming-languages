aeaf7b77cf4614f2369c145eaea38e993cb18927
Aleksandr <asp1rantzenevich@gmail.com>
2022-09-27 16:35:22 +0300

    #

---
+++ Labs/Lab04/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите номер задания: ");
+            int p = Convert.ToInt32(Console.ReadLine());
+            switch (p)
+            {
+                case 1:
+                {
+                    double proizv = 0.0;
+                    int null_one = 0, null_two = 0;
+                    int[] arr = new int[] { 23, -2, 4, 785, 89, 0, 14, 214, 64, 0, 32, 56, 87, 11, 0, 23, 55 };
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        if (arr[i] == 0)
+                        {
+                            null_one = i;
+                            break;
+                        }
+                    }
+
+                    for (int i = null_one + 1; i < arr.Length; i++)
+                    {
+                        if (arr[i] == 0)
+                        {
+                            null_two = i;
+                            break;
+                        }
+                    }
+
+                    if (null_two != 0)
+                    {
+                        for (int i = null_one + 1; i < null_two; i++)
+                        {
+                            proizv *= arr[i];
+                        }
+                    }
+
+                    Console.WriteLine($"Произведение: {proizv}");
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("Exit...");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Practice/Prac01/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            try
+            {
+                Console.Write("Введите размер массива: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int[] arr = new int[n];
+                for (int i = 0; i < n; i++)
+                {
+                    arr[i] = rnd.Next(-100, 100);
+                }
+                Console.Write("1 - максимальный элемент массива;\n2 - сумма элементов массива, " +
+                              "расположенных до последнего положительного элемента;\n3 - сжать массив, " +
+                              "удалив из него все элементы, модуль которых находится в интервале [a, b]. " +
+                              "Освободившееся в конце массива элементы заполняются нулями." +
+                              "\nВведите номер действия: ");
+                int p = Convert.ToInt32(Console.ReadLine());
+                switch (p)
+                {
+                    case 1:
+                    {
+                        int max = 0;
+                        for (int i = 0; i < n; i++)
+                        {
+                            if (arr[i] > max)
+                            {
+                                max = arr[i];
+                            }
+                        }
+                        Console.WriteLine($"Ответ: {max}");
+                        break;
+                    }
+                    case 2:
+                    {
+                        int sum = 0;
+                        int k = n;
+                        while (arr[k] < 0)
+                        {
+                            k--;
+                        }
+                        for (int i = 0; i < k; i++)
+                        {
+                            sum += arr[i];
+                        }
+                        Console.WriteLine($"Ответ: {sum}");
+                        break;
+                    }
+                    case 3:
+                    {
+                        Console.Write("Введите A: ");
+                        int A = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Введите B: ");
+                        int B = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0, j = 0; i < n; i++)
+                        {
+                            if (A <= Math.Abs(arr[i]) && Math.Abs(arr[i]) <= B)
+                            {
+                                for (j = i; j < n - 1; j++)
+                                {
+                                    arr[j] = arr[j + 1];
+                                }
+                                arr[j] = 0;
+                            }
+                        }
+
+                        for (int i = 0; i < n; i++)
+                        {
+                            Console.WriteLine(arr[i] + '\t');
+                        }
+                        break;
+                    }
+                    default:
+                    {
+                        Console.WriteLine("Exit...");
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+        }
+    }
+}
