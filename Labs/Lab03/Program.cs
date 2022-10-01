e93a0405c9a3eb611386b7314a311598d2cc92c1
Aleksandr <asp1rantzenevich@gmail.com>
2022-10-01 03:44:50 +0300

    #

--- Labs/Lab03/Program.cs
+++ Labs/Lab03/Program.cs
@@ -35,21 +35,79 @@ namespace Lab03
                 }
                 case 2:
                 {
-                    int a, i = 100;
-                    while (i <= 700)
+                    int i=100;
+                    do
                     {
-                        a = i / 100;
-                        if (a % 2 != 0)
-                        {
-                            Console.WriteLine(i);
-                        }
-                        i++;
-                    }
+                        Console.WriteLine(i);
+                            
+                        i += 200;
+                    } while (i < 800);
                     break;
                 }
                 case 3:
                 {
-
+                    int s = 0;
+                    string q = default, r = default;
+                    for (int i = 1; i < 100; i++)
+                        if ((q = (i * i).ToString()) == (r = new string(q.Reverse().ToArray())))
+                            Console.WriteLine($"palindrome {++p} q={q} == r={r}"); ;
+                    Console.WriteLine($"total: {s}");
+                    break;
+                }
+                case 4:
+                {
+                    Console.Write("Введите x: ");
+                    int x = Convert.ToInt32(Console.ReadLine());
+                    int N = -1;
+                    do
+                    {
+                        Console.Write("Введите N (N > 0): ");
+                        N = Convert.ToInt32(Console.ReadLine());
+                    } while (N < 0);
+                    double t = Math.Cos(x);
+                    double sum = t;
+                    for (int i = 1; i < N; i++)
+                    {
+                        t = t * x * x / i;
+                        sum += t * (2 * i + 1);
+                    }
+                    Console.WriteLine($"Сумма: {sum}");
+                    break;
+                }
+                case 5:
+                {
+                    for (double i = 0.1; i <= 2.2; i+= .2)
+                    {
+                        Console.WriteLine($"x = {i}\ny = {f(i)}");
+                    }
+                    double f(double x)
+                    {
+                        return Math.Pow(x, 2) - Math.Pow(Math.Cos(x + 1), 2);
+                    }
+                    break;
+                }
+                case 6:
+                {
+                    for (double i = -2; i <= 3; i+= .2)
+                    {
+                        Console.WriteLine($"x = {i}\ny = {f(i)}");
+                    }
+                    double f(double x)
+                    {
+                        if (x > 1.5)
+                        {
+                            return Math.Pow(2.5, 3) + 6 * Math.Pow(x, 2) - 30;
+                        }
+                        else if (0 <= x && 0 <= 1.5)
+                        {
+                            return x + 1;
+                        }
+                        else if (x < 0)
+                        {
+                            return x;
+                        }
+                        return 0;
+                    }
                     break;
                 }
                 default:
