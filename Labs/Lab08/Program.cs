6f5322d2c565dc89ebf223199cea8cd18e910902
Aleksandr <asp1rantzenevich@gmail.com>
2022-10-30 02:46:53 +0300

    #

---
+++ Labs/Lab08/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите номер: ");
+            var p = Convert.ToInt32(Console.ReadLine());
+            switch (p)
+            {
+                case 1:
+                {
+                    Shar shr = new Shar(3);
+                    Console.WriteLine(shr.Area());
+                    Console.WriteLine(shr.Volume());
+                    break;
+                }
+                default:
+                {
+                    Console.WriteLine("Exit...");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Labs/Lab08/Shar.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab08
+{
+    public class Shar
+    {   
+        private readonly int _r;
+        
+        public Shar(){}
+        public Shar(int r)
+        {
+            try
+            {
+                this._r = r;
+                if (r < 1)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            catch (OutOfMemoryException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public double Area()
+        {
+            try
+            {
+                return Math.Pow(_r, 2) * Math.PI;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            catch (StackOverflowException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public double Volume()
+        {
+            try
+            {
+                return (4.0 / 3.0) * Math.PI * Math.Pow(_r, 3);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            catch (StackOverflowException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
