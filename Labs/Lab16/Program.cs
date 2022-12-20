b8c684437f7b0aa361ec38f66bd097a89b456282
Aleksandr <asp1rantzenevich@gmail.com>
2022-12-20 04:34:43 +0300

    #

---
+++ Labs/Lab16/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab16_dll;
+
+namespace Lab16
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("-------Объект 1-------");
+            var triangle = new Triangle
+            {
+                A = 2.4,
+                B = 3.6,
+                C = 5.0
+            };
+            Console.WriteLine(triangle.ToString());
+            Console.WriteLine("-------Объект 2-------");
+            var triangle2 = new Triangle(2.9, 2.6, 3.2);
+            Console.WriteLine($"Стороны: A - {triangle2.A}; B - {triangle2.B}; C - {triangle2.C}" +
+                              $"\nПериметр: {triangle2.Perimeter()}" +
+                              $"\nПлощадь: {triangle2.Square()}");
+            Console.WriteLine("-------Объект 3-------");
+            var triangle3 = new Triangle(-2.9, 2.6, 3.2);
+        }
+    }
+}
---
+++ Labs/Lab16_dll/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab16_dll
+{
+    public class Triangle : IPrintable
+    {
+        public double A { set; get; }
+
+        public double B { set; get; }
+
+        public double C { set; get; }
+
+        public Triangle () {}
+        public Triangle (double a, double b, double c)
+        {
+            if ((a > 0.0 && b > 0.0 && c > 0.0) // check for positive
+                && ((b + c) > a // condition check: the sum of any two sides must be greater than the third
+                    || (a + c) > b
+                    || (a + b) > c)
+                && (a is double) && (b is double) && (c is double)) // type checking
+            {
+                this.A = a;
+                this.B = b;
+                this.C = c;
+            }
+            else
+            {
+                throw new Exception("Стороны должны соответствовать условию:" +
+                                    "\nСтороны вещественные и положительные," +
+                                    "\nCумма двух любых сторон должна быть больше третьей!");
+            }
+            this.A = a;
+            this.B = b;
+            this.C = c;
+        }
+
+        public double Perimeter () => this.A + this.B + this.C;
+
+        public double Square()
+        {
+            var p = (this.A + this.B + this.C) / 2;
+            return Math.Sqrt(p * (p - this.A) * (p - this.B) * (p - this.C));
+        }
+
+        public override string ToString()
+        {
+            return "Информация об объекте: " +
+                   $"\nСтороны: A - {this.A}; B - {this.B}; C - {this.C}" +
+                   $"\nПериметр: {Perimeter()}" +
+                   $"\nПлощадь: {Square()}";
+        }
+    }
+
+    internal interface IPrintable
+    {
+        string ToString();
+    }
+}
