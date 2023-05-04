bbedc2d5283fd50f91823b5847cad934b4be4739
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2023-05-05 00:33:33 +0300

    #

---
+++ Practice/Articles/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+
+namespace Articles
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string articleFilePath = "article.txt"; // путь к файлу со статьей
+        private readonly string articleImagePath = "articleImage.png"; // путь к файлу с изображением из статьи
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeComponent();
+            LoadArticle();
+            LoadImage();
+        }
+
+        private void LoadArticle()
+        {
+            // загрузка текста статьи из файла
+            string articleText = File.ReadAllText(articleFilePath);
+            ArticleText.Text = articleText;
+        }
+
+        private void LoadImage()
+        {
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            image.UriSource = new Uri(articleImagePath, UriKind.RelativeOrAbsolute);
+            image.EndInit();
+            ArticleImage.Source = image;
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            string articleText = ArticleText.Text;
+            File.WriteAllText("fileSave.txt", articleText);
+            MessageBox.Show("Файл успешно сохранён!");
+        }
+
+        private void Print_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+                FlowDocument document = new FlowDocument(new Paragraph(new Run(ArticleText.Text)));
+                printDialog.PrintDocument(((IDocumentPaginatorSource)document).DocumentPaginator, "Статья");
+            }
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            string searchData = SearchTextBox.Text;
+            string articleText = ArticleText.Text;
+            if (string.IsNullOrEmpty(searchData) || string.IsNullOrEmpty(ArticleText.Text))
+            {
+                MessageBox.Show("Введите данные для поиска и загрузите статью");
+                return;
+            }
+
+            int index = articleText.IndexOf(searchData, StringComparison.CurrentCultureIgnoreCase);
+            if (index >= 0)
+            {
+                MessageBox.Show("Найдено");
+                ArticleText.Inlines.Clear();
+
+                while (index >= 0)
+                {
+                    // добавить Run с текстом до найденного слова
+                    ArticleText.Inlines.Add(new Run(articleText.Substring(0, index)));
+
+                    // добавить Run с найденным словом
+                    Run highlightRun = new Run(articleText.Substring(index, searchData.Length));
+                    highlightRun.Background = Brushes.Yellow;
+                    highlightRun.Foreground = Brushes.Black;
+                    ArticleText.Inlines.Add(highlightRun);
+
+                    // обновить оставшийся текст и продолжить поиск
+                    articleText = articleText.Substring(index + searchData.Length);
+                    index = articleText.IndexOf(searchData, StringComparison.CurrentCultureIgnoreCase);
+                }
+
+                // добавить оставшийся текст после последнего найденного слова
+                if (!string.IsNullOrEmpty(articleText))
+                {
+                    ArticleText.Inlines.Add(new Run(articleText));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Не найдено");
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            // закрытие текущего окна и переход на главное окно
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            // закрытие приложения
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ Practice/Graphing/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Graphing
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PlotButton_Click(object sender, RoutedEventArgs e)
+        {
+            canvas.Children.Clear();
+
+            string formula = formulaTextBox.Text;
+
+            double minX = -5;
+            double maxX = 5;
+
+            double xStep = 0.1;
+            for (double x = minX; x <= maxX; x += xStep)
+            {
+                double y = CalculateYValue(formula, x);
+
+                // plot the point
+                double pointSize = 2;
+                Ellipse point = new Ellipse
+                {
+                    Width = pointSize,
+                    Height = pointSize,
+                    Fill = Brushes.Black
+                };
+                Canvas.SetLeft(point, x * 50 + 250);
+                Canvas.SetBottom(point, y * 50 + 175);
+                canvas.Children.Add(point);
+            }
+
+            Polyline polyline = new Polyline
+            {
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            };
+            for (double x = minX; x <= maxX; x += xStep)
+            {
+                double y = CalculateYValue(formula, x);
+                polyline.Points.Add(new Point(x * 50 + 250, y * 50 + 175));
+            }
+            canvas.Children.Add(polyline);
+
+            formulaTextBlock.Text = "f(x) = " + formula;
+        }
+
+        private double CalculateYValue(string formula, double x)
+        {
+            return Math.Sin(x);
+        }
+    }
+}
